3fcdf6b2cdd2fcad2d5ae0d779ba2a08aa4de082
Ashraf Kamarudin <ashrafkamarudin1995@gmail.com>
2020-05-22 00:09:23 +0800

    feat: add cache

--- lib/flutter_cache.dart
+++ lib/flutter_cache.dart
@@ -1,7 +1,77 @@
 library flutter_cache;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class Cache {
+
+  static Future remember (var data, String key, [int expiredAt]) async {
+    try {
+      var x;
+      data is List
+        ? x = await Cache.load(key, true)
+        : x = await Cache.load(key);
+
+        if (x == null) {
+          throw('Null');
+        } else {
+          return x;
+        }
+    } catch (e) {
+      expiredAt == null 
+        ?  Cache.overwrite(data, key)
+        :  Cache.overwrite(data, key, expiredAt);
+    }
+  }
+
+  static Future<void> overwrite (var data, String key, [int expiredAt]) async {
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    if (data is Map) prefs.setString(key, json.encode(data));
+    if (data is List) {
+      print('save list');
+      List<String> list = data.map((i) => json.encode(i.toMap())).toList();
+      prefs.setStringList(key, list);
+    }
+    if (data is String) prefs.setString(key, data);
+
+    if (expiredAt != null) prefs.setString(key + 'ExpiredAt', (Cache._currentTimeInSeconds() + expiredAt).toString());
+  }
+
+  static Future load (String key, [bool list = false]) async {
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String cacheData        = prefs.getString(key + 'ExpiredAt');
+
+    if (cacheData != null) {
+      int time = int.parse(cacheData);
+      if (time < Cache._currentTimeInSeconds()) {
+        prefs.remove(key + 'ExpiredAt');
+        prefs.remove(key);
+      }
+    }
+
+    if (list) {
+      print('list');
+      print(prefs.getStringList(key));
+      return prefs.getStringList(key);
+    }
+
+    return json.decode(prefs.getString(key));
+  }
+
+  static Future clear () async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.clear();
+  }
+
+  static Future destroy (String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove(key);
+  }
+
+  static int _currentTimeInSeconds() { // private
+    var ms = (new DateTime.now()).millisecondsSinceEpoch;
+    return (ms / 1000).round();
+  }
 }
