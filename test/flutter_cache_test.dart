321dcb3e772bf65caf65a1ed98d4cf24c4a07c81
Ashraf Kamarudin <ashrafkamarudin1995@gmail.com>
2020-05-22 01:20:27 +0800

    feat: add tests

--- test/flutter_cache_test.dart
+++ test/flutter_cache_test.dart
@@ -1,13 +1,73 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_cache/flutter_cache.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  test('Can Cache by String', () async {
+    /* Begin Setup Test */
+    SharedPreferences.setMockInitialValues({});
+    await Cache.remember('data', 'key');
+    /* End Setup Test */
+
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    expect(pref.getString('key'), 'data');
   });
+
+  test('Can Cache by Map', () async {
+    /* Begin Setup Test */
+    SharedPreferences.setMockInitialValues({});
+    Map data = {
+      'name' : 'Ashraf Kamarudin',
+      'job' : 'Programmer'
+    };
+    await Cache.remember(data, 'key');
+    /* End Setup Test */
+
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    expect(json.decode(pref.getString('key')), data);
+  });
+
+  test('Can Load by String', () async {
+    /* Begin Setup Test */
+    SharedPreferences.setMockInitialValues({});
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setString('key', 'data');
+    /* End Setup Test */
+
+    expect(await Cache.load('key'), 'data');
+  });
+
+  test('Can Load by Map', () async {
+    /* Begin Setup Test */
+    SharedPreferences.setMockInitialValues({});
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    Map data = {
+      'name' : 'Ashraf Kamarudin',
+      'job' : 'Programmer'
+    };
+    pref.setString('key', json.encode(data));
+    /* End Setup Test */
+
+    /* Test */
+    expect(await Cache.load('key'), data);
+  });
+
+   test('Will Retrieve Cache instead of Caching if Cache Exist', () async {
+    /* Begin Setup Test */
+    SharedPreferences.setMockInitialValues({});
+    Map data = {
+      'name' : 'Ashraf Kamarudin',
+      'job' : 'Programmer'
+    };
+    await Cache.remember('data', 'key'); // for string
+    await Cache.remember(data, 'map'); // for map
+    /* End Setup Test */
+
+    expect(await Cache.remember('data', 'key'), 'data'); // test for string
+    expect(await Cache.remember(data, 'map'), data); // test for map
+  });
+
 }
