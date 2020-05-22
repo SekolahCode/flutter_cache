26c66e3c126df80aa4926f8f63bc9e038f60d623
Ashraf Kamarudin <ashrafkamarudin1995@gmail.com>
2020-05-23 06:14:57 +0800

    feat: ran flutter format

--- lib/Parse.dart
+++ lib/Parse.dart
@@ -1,35 +1,22 @@
 import 'dart:convert';
 
 class Parse {
-
-  static content (var data) {
+  static content(var data) {
     /* @type String */
-    if (data is String) return {
-      'content' : data,
-      'type'    : 'String'
-    };
+    if (data is String) return {'content': data, 'type': 'String'};
 
     /* @type Map */
-    if (data is Map) return {
-      'content' : jsonEncode(data),
-      'type'    : 'Map'
-    };
+    if (data is Map) return {'content': jsonEncode(data), 'type': 'Map'};
 
     /* @type List<String> */
     if (data is List<String>) {
-      return {
-        'content' : data,
-        'type'    : 'List<String>'
-      };
+      return {'content': data, 'type': 'List<String>'};
     }
 
     /* @type List<Map> */
     if (data is List<Map>) {
       List<String> list = data.map((i) => jsonEncode(i)).toList();
-      return {
-        'content' : list,
-        'type'    : 'List<Map>'
-      };
+      return {'content': list, 'type': 'List<Map>'};
     }
 
     throw ('Unsupported Data Type');
